e691319dd187dfc31efc6a846f2282b36f76a507
github username <github email address>
2020-05-20 21:42:25 +0100

    commented out everything related to reach router and the navBar to actually get the app running

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { Router } from "@reach/router";
+// import { Router } from "@reach/router";
 // import Home from "./components/home /home";
 // import About from "./components/about/about";
 
 function App() {
   return (
     <div>
-      <Router>{/* <Home path="/" />
-        <About path="about" /> */}</Router>
+      {/* <Router><Home path="/" />
+        <About path="about" /></Router> */}
     </div>
   );
 }
--- src/components/home /home.tsx
+++ src/components/home /home.tsx
@@ -1,21 +1,17 @@
 import React from "react";
-import Nav from "../navBar/navBar";
-// import { StyledH1, StyledH2 } from "./home.styled";
+// import Nav from "../navBar/navBar";
+import { StyledH1, StyledH2 } from "./home.styled";
+import "../../App.css";
 
-const Home = () => (
-  <div>
-    <Nav />
-    Home
-  </div>
-);
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <StyledH1>Welcome to Typescript</StyledH1>
-//         <StyledH2> A guide to learn Typescript </StyledH2>
-//       </header>
-//     </div>
-//   );
-// }
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <StyledH1>Welcome to Typescript</StyledH1>
+        <StyledH2> A guide to learn Typescript </StyledH2>
+      </header>
+    </div>
+  );
+}
 
 export default Home;
